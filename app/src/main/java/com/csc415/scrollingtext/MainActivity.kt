107514b9f74b79787e32ee4cbf1321aad73aae43
Will St. Onge <ongewill@gmail.com>
2021-09-17 22:34:29 -0400

    Initialize

---
+++ app/src/main/java/com/csc415/scrollingtext/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.csc415.scrollingtext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
